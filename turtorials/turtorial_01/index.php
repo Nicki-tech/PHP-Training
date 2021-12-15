2b92fc585c14e68320c456a11ccbdf7b8d684949
Acer <yunthiri1998@gmail.com>
2021-12-15 11:50:41 +0630

    turtorial_01 uploaded

---
+++ turtorials/turtorial_01/index.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Document</title>
+</head>
+<body>
+<h3 style="text-align: center;">Chess Board</h3>
+  <table style="margin: 0 auto;">
+    <?php
+      for($row = 1; $row <= 8; $row++){
+         echo "<tr>";
+            for($column = 1; $column <= 8; $column++){
+               if($column % 2 == 0){
+                  $color = '#ffffff';
+               } else {
+                  $color = '#000000';
+               } 
+               if($row %2 ==0){
+                  if($column %2 ==0){
+                     $color = '#000000';
+                  }
+                  else {
+                  $color = '#ffffff';
+                   } 
+               }
+      echo "<td style = 'border:1px solid;height:90px;width:90px;background-color:".$color."'></td>";
+            }
+            echo "</tr>";     
+      }
+   ?>
+</table>
+</body>
+</html>
