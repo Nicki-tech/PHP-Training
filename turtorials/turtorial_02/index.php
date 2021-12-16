82fb6eabd8cc21f3418f2bc5ec6a051dbfeaabc6
Acer <yunthiri1998@gmail.com>
2021-12-16 12:47:34 +0630

    [master]turtorial-02 updated

--- turtorials/turtorial_02/index.php
+++ turtorials/turtorial_02/index.php
@@ -1,31 +1,34 @@
 <!DOCTYPE html>
 <html lang="en">
-<head>
-  <meta charset="UTF-8">
-  <meta http-equiv="X-UA-Compatible" content="IE=edge">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>Document</title>
-</head>
-<body>
-<?php
-echo "<pre>";
-$n = 6;
-for ($i = 1; $i < 6; $i++) {
-    for ($j = $i; $j < 6; $j++)
-        echo "&nbsp;&nbsp;";
-    for ($j = 2 * $i - 1; $j > 0; $j--)
-        echo ("&nbsp;*");
-    echo "<br>";
-}
 
-for ($i = 6; $i > 0; $i--) {
-    for ($j = $n - $i; $j > 0; $j--)
-        echo "&nbsp;&nbsp;";
-    for ($j = 2 * $i - 1; $j > 0; $j--)
-        echo ("&nbsp;*");
-    echo "<br>";
-}
-echo "</pre>";
-?>
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Turtorial_02</title>
+</head>
+
+<body>
+    <?php
+    echo "<pre>";
+    $n = 6;
+    for ($i = 1; $i < 6; $i++) {
+        for ($j = $i; $j < 6; $j++)
+            echo "&nbsp;&nbsp;";
+        for ($j = 2 * $i - 1; $j > 0; $j--)
+            echo ("&nbsp;*");
+        echo "<br>";
+    }
+
+    for ($i = 6; $i > 0; $i--) {
+        for ($j = $n - $i; $j > 0; $j--)
+            echo "&nbsp;&nbsp;";
+        for ($j = 2 * $i - 1; $j > 0; $j--)
+            echo ("&nbsp;*");
+        echo "<br>";
+    }
+    echo "</pre>";
+    ?>
 </body>
+
 </html>
