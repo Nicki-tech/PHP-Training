6dbb70bf651911d13dcdca372d16f461cf5d4723
Acer <yunthiri1998@gmail.com>
2021-12-20 11:45:36 +0630

    [master]turtorial_05 updated

--- turtorials/turtorial_04/login.php
+++ turtorials/turtorial_04/login.php
@@ -17,15 +17,12 @@
 </head>
 
 <body>
-
   <form action="index.php" method="post">
     <p>
       Username: <input name="username" type="text" value="<?php if (isset($_COOKIE["username"])) {
-                                                            echo $_COOKIE["username"];
                                                           } ?>" class="input-field">
     </p>
     <p>Password: <input name="password" type="password" value="<?php if (isset($_COOKIE["password"])) {
-                                                                  echo $_COOKIE["password"];
                                                                 } ?>" class="input-field">
     </p>
     <p><input type="checkbox" name="remember" /> Remember me
