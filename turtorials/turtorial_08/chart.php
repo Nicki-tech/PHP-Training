941f50484af1b27a6c9779605df8ddba580d79dc
Acer <yunthiri1998@gmail.com>
2021-12-23 16:46:59 +0630

    [master]turtorial-_09 uploded

--- turtorials/turtorial_08/chart.php
+++ turtorials/turtorial_08/chart.php
@@ -20,7 +20,6 @@ echo "<script>var data=" . json_encode($data) . ";</script>";
   <meta charset="UTF-8">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <link rel="stylesheet" href="css/style.css">
   <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.css">
   <script src="js/chart.min.js"></script>
   <script src="js/jquery-3.6.0.min.js"></script>
